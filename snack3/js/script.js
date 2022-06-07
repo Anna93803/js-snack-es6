063dca393d3016a3a1f3b29e81cf27d6730e2cd1
Anna93803 <anna.tassone93@gmail.com>
2022-06-07 15:42:48 +0200

    add snack3

---
+++ snack3/js/script.js
@@ -0,0 +1,40 @@
+// Snack 3
+
+// Dato l’array di nomi:
+// const myArray = [‘Michele’, ‘Fabio’, ‘Roberto’, ‘Giovanni’, ‘Simone’, ‘Chiara’];
+
+const myArray = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
+
+console.log(myArray);
+
+// e dati due numeri min e max (min più piccolo di max).
+let numeriMin = 3;
+let numeriMax = 5;
+// Creiamo un nuovo array contenente i valori che hanno la posizione compresa tra i due numeri.
+// Eseguiamo questo esercizio prima con forEach e poi con filter.
+
+const nuovoArray = [];
+
+// CON CICLO FOREACH
+
+myArray.forEach((element, i) => {
+
+    element = myArray;
+    if(i >= numeriMin && i <= numeriMax){
+        nuovoArray.push(element[i]);
+    }
+});
+
+console.log(`Con Ciclo ForEach ${nuovoArray}`);
+
+// CON CICLO FILTER
+
+const array = myArray.filter((element, i) => {
+
+    element = myArray;
+    if(i >= numeriMin && i <= numeriMax){
+        nuovoArray.push(element[i]);
+    }
+});
+
+console.log(`Con Ciclo Filter ${nuovoArray}`);
